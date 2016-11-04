c40533be332b60fa4ebfa10eba95f4f5e03a17a4
Septi Maulina <septimaulina17@gmail.com>
2016-11-04 22:29:53 +0700

    navbar colapsible and load the wordpress pages

---
+++ wp-content/themes/wpbootstrap/navbar.php
@@ -0,0 +1,34 @@
+<!-- this is the main home navbar  -->
+  <div class="navbar navbar-inverse">
+  <div class="navbar-inner">
+    <div class="container">
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+      <a class="brand" href="#">Project name</a>
+      <div class="nav-collapse collapse">
+        <ul class="nav">
+            <?php wp_list_pages(array('title_li' => '')); ?>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </div>
+</div>
+
+
+  <ul id="menu" class="nav nav-pills navbar-top">
+    
+
+    <li role="presentation" class="disabled">
+              <a href="#">Support Line : +62 274 555666</a>
+    </li>
+
+    <li role="presentation" class="disabled"><a href="#">Email : suppoer@kokijoni.com</a></li>
+    
+    <li role="presentation" class="disabled" ><a href="#" class="">Email : suppoer@kokijoni.com</a></li>
+    
+
+    
+  </ul>
