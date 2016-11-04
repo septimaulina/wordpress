418c636a658468dc5d3ee6fa38e4eefdda5975a7
Septi Maulina <septimaulina17@gmail.com>
2016-11-05 01:02:27 +0700

    commit navbar title and menu done. navbar top not finished yet

--- wp-content/themes/wpbootstrap/header.php
+++ wp-content/themes/wpbootstrap/header.php
@@ -15,8 +15,10 @@
     <?php wp_head(); ?>
   </head>
   <body>
+<!-- this is navbar support line and contact -->
 
-  <div class="navbar navbar-inverse navbar-fixed-top">
+ -->
+   <div class="navbar navbar-default navbar-fixed-top">
     <div class="navbar-inner">
       <div class="container">
         <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
@@ -24,7 +26,40 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </a>
-        <a class="brand" href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a>
+        <div class="nav-collapse collapse">
+          <ul id="menu" class="nav nav-pills">
+
+              <li role="presentation" class="disabled">
+                        <a href="#" class="v4-tease">Support Line : +62 274 555666</a>
+              </li>
+
+              <li role="presentation" class="disabled"><a href="#">Email : suppoer@kokijoni.com</a></li>
+              
+              <li role="presentation" class="disabled" ><a href="#" class="">Email : suppoer@kokijoni.com</a></li>
+
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<!-- this is navbar brand title -->
+<div class="bs-docs-header" id="content" tabindex="-1">
+    <div class="container">
+      <h1>KOKI JONI</h1>
+    </div>
+</div>
+
+<!-- this is navbar menu -->
+  <div class="navbar navbar-default navbar-static-top">
+    <div class="navbar-inner">
+      <div class="container">
+        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </a>
+        <!-- <a class="brand" href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a> -->
         <div class="nav-collapse collapse">
           <ul class="nav">
 
